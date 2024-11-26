98a4c811658c39ebbb4f456a2033ca0653f93d66
innTall <avg999.trade@gmail.com>
2024-11-26 06:00:02 +0100

    Margin orders - new disable logic

--- src/views/BybitMarginResult.vue
+++ src/views/BybitMarginResult.vue
@@ -34,7 +34,7 @@ watch(
 // Handle the first click on the 'sl' field
 const handleSLFocus = () => {
 	if (isFirstClickOnSL.value) {
-		store.addNewBlock(); // Add a new block
+		addNewBlock(); // Add a new block
 		isFirstClickOnSL.value = false; // Prevent subsequent additions
 	}
 };
@@ -58,6 +58,19 @@ const totalActiveSum = computed(() => {
 		return sum; // If no activeValue, add nothing
 	}, 0);
 });
+
+// Toggle block disable state
+const toggleBlock = (block, disable) => {
+	block.disabled = disable;
+};
+// Handle TP or SL activation
+const handleTPorSLActivation = (block) => {
+	toggleBlock(block, true);
+};
+// Reactivate the block via double-click
+const handleDoubleClick = (block) => {
+	toggleBlock(block, false);
+};
 </script>
 
 <template>
@@ -77,8 +90,9 @@ const totalActiveSum = computed(() => {
 		</div>
 		<!-- Loop through blocks -->
 		<div v-for="(block, index) in orderBlocks" :key="index" class="">
-			<div class="relative flex gap-3">
-				<button v-if="index && block.activeValue === 'set'" @click="removeBlock(index)"
+			<div class="relative flex gap-3" @dblclick="handleDoubleClick(block)">
+				<!-- Remove Delete Button if block is disabled -->
+				<button v-if="index && block.activeValue === 'set' && !block.disabled" @click="removeBlock(index)"
 					class="w-5 bg-red-600 text-white hover:bg-red-500 hover:text-white">
 					X
 				</button>
@@ -89,21 +103,23 @@ const totalActiveSum = computed(() => {
 						<div class="flex gap-3">
 							<label :for="'set' + index" :class="{
 								'text-white font-bold': block.activeValue === 'set',
-								'text-gray-500': block.activeValue !== 'set',
+								'text-gray-700': block.activeValue !== 'set',
 							}">
-								<input :id="'set' + index" type="radio" value="set" v-model="block.activeValue" />
+								<input :id="'set' + index" type="radio" value="set" v-model="block.activeValue"
+									:disabled="block.disabled" />
 								Set
 							</label>
 							<label :for="'profit' + index" :class="{
 								'text-green-500 font-bold': block.activeValue === 'profit',
-								'text-gray-500': block.activeValue !== 'profit',
+								'text-gray-700': block.activeValue !== 'profit',
 							}">
-								<input :id="'profit' + index" type="radio" value="profit" v-model="block.activeValue" />
+								<input :id="'profit' + index" type="radio" value="profit" v-model="block.activeValue"
+									:disabled="block.disabled" @change="handleTPorSLActivation(block)" />
 								TP
 							</label>
 							<span :class="{
 								'text-green-500': block.activeValue === 'profit',
-								'text-gray-500': block.activeValue !== 'profit',
+								'text-gray-700': block.activeValue !== 'profit',
 							}">
 								{{ T_P(block.buy, block.amnt, block.tp) }}
 							</span>
@@ -111,14 +127,15 @@ const totalActiveSum = computed(() => {
 							<!-- Loss Radio Button and Label -->
 							<label :for="'loss' + index" :class="{
 								'text-red-500 font-bold': block.activeValue === 'loss',
-								'text-gray-500': block.activeValue !== 'loss',
+								'text-gray-700': block.activeValue !== 'loss',
 							}">
-								<input :id="'loss' + index" type="radio" value="loss" v-model="block.activeValue" />
+								<input :id="'loss' + index" type="radio" value="loss" v-model="block.activeValue"
+									:disabled="block.disabled" @change="handleTPorSLActivation(block)" />
 								SL
 							</label>
 							<span :class="{
 								'text-red-500': block.activeValue === 'loss',
-								'text-gray-500': block.activeValue !== 'loss',
+								'text-gray-700': block.activeValue !== 'loss',
 							}">
 								{{ S_L(block.buy, block.amnt, block.sl) }}
 							</span>
@@ -130,28 +147,27 @@ const totalActiveSum = computed(() => {
 			<div class="flex justify-between">
 				<input :id="'date' + index" type="date" v-model="block.date" placeholder="Date"
 					class="w-[8ch] bg-gray-900 text-center appearance-none" @focus="clearField(date)"
-					:disabled="block.activeValue !== 'set'" />
+					:disabled="block.disabled" />
 				<input :id="'symbol' + index" type="text" v-model="block.symbol" placeholder="Symbol"
 					class="w-[6ch] bg-gray-900 text-center uppercase" @focus="clearField(symbol)"
-					:disabled="block.activeValue !== 'set'" />
+					:disabled="block.disabled" />
 				<input :id="'buy' + index" type="number" v-model="block.buy" placeholder="Buy"
 					class="w-[8ch] bg-gray-900 text-center text-blue-400 appearance-none" @focus="clearField(buy)"
-					:disabled="block.activeValue !== 'set'" />
+					:disabled="block.disabled" />
 				<input :id="'amnt' + index" type="number" v-model="block.amnt" placeholder="Amnt"
 					class="w-[6ch] bg-gray-900 text-center appearance-none" @focus="clearField(amnt)"
-					:disabled="block.activeValue !== 'set'" />
+					:disabled="block.disabled" />
 				<input :id="'tp' + index" type="number" v-model="block.tp" placeholder="TP"
-					class="w-[8ch] bg-gray-900 text-center text-green-400 appearance-none" :class="{
+					class="w-[8ch] bg-gray-900 text-center appearance-none" :class="{
 						'text-green-400': block.activeValue === 'profit',
-						'text-gray-500': block.activeValue === 'loss' || block.activeValue !== 'set'
-					}" @focus="clearField(tp)" :disabled="block.activeValue !== 'set'" />
+						'text-gray-700': block.activeValue !== 'profit'
+					}" @focus="clearField('tp')" :disabled="block.disabled" />
 				<!-- Attach focus listener to the 'sl' input -->
 				<input :id="'sl' + index" type="number" v-model="block.sl" placeholder="SL"
-					class="w-[8ch] bg-gray-900 text-center text-red-400 appearance-none" 
-					:class="{
+					class="w-[8ch] bg-gray-900 text-center appearance-none" :class="{
 						'text-red-400': block.activeValue === 'loss',
-						'text-gray-500': block.activeValue === 'profit' || block.activeValue !== 'set'
-					}" @focus="clearField(sl); handleSLFocus()" :disabled="block.activeValue !== 'set'" />
+						'text-gray-700': block.activeValue !== 'loss'
+					}" @focus="clearField('sl'); handleSLFocus()" :disabled="block.disabled" />
 			</div>
 			<hr class="border-green-600 mt-2" />
 		</div>
