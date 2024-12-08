bc02816bf4569c14f87c7fc7400570641b10f86c
innTall <avg999.trade@gmail.com>
2024-12-08 12:16:08 +0100

    Rewrite with Pinia

--- src/stores/spotBinance.js
+++ src/stores/spotBinance.js
@@ -1,131 +1,142 @@
 import { defineStore } from "pinia";
-import { reactive, ref, computed } from "vue";
+import { ref, reactive, watch } from "vue";
 
 export const useSpotBinanceStore = defineStore("spotBinance", () => {
   const fee = 0.001;
   const deposit = ref(468);
-  const coefOfRisk = ref(0.02);
-  const coefNextOrderCost = ref(1.2);
-  let nextBlockId = 1;
+  const riskTrade = ref(0.02);
+  const coefNextBuyOrder = ref(1.2);
 
-  const firstOrderCost = computed(
-    () => +(deposit.value * coefOfRisk.value).toFixed(2)
-  );
-  const activeBlocks = ref([createNewBlock()]);
+  const defaultBlock = () => ({
+    nr: 1,
+    symbol: "",
+    start: null,
+    fin: null,
+    buyPrice: 1,
+    sellPrice: null,
+    orders: reactive([
+      {
+        buyPrice: 1,
+        sellPrice: null,
+        amount: null,
+        buyOrder: null,
+        sellOrder: null,
+        profit: null,
+      },
+    ]),
+  });
 
-  function generateUniqueId() {
-    return nextBlockId++;
-  }
+  const orderBlocks = ref([defaultBlock()]);
 
-  function createNewBlock() {
-    const id = generateUniqueId();
-    const block = reactive({
-      id,
-      symbol: "",
-      open: "",
-      close: "",
-      orders: [],
-      isSaved: false,
-      summary: computed(() => {
-        const totalTokenAmount = block.orders.reduce(
-          (sum, order) => sum + order.tokenAmount,
-          0
-        );
-        const totalBuyOrder = block.orders.reduce(
-          (sum, order) => sum + order.buyOrder,
-          0
-        );
-        const totalSellOrder = block.orders.reduce(
-          (sum, order) => sum + order.sellOrder,
-          0
-        );
-        const totalProfit =
-          block.orders.length > 0
-            ? block.orders[block.orders.length - 1].profit
-            : 0;
-        return {
-          avgBuyPrice: totalTokenAmount ? totalBuyOrder / totalTokenAmount : 0,
-          avgSellPrice: totalTokenAmount
-            ? totalSellOrder / totalTokenAmount
-            : 0,
-          totalProfit,
-          totalBuyOrders: totalBuyOrder,
-          totalTokenAmount,
-        };
-      }),
+  const addOrderBlock = () => {
+    orderBlocks.value.push({
+      ...defaultBlock(),
+      nr: orderBlocks.value.length + 1,
     });
+  };
 
-    const initialOrder = {
-      id: 1,
-      buyPrice: 1.2345,
-      sellPrice: 1.456,
-      tokenAmount: 0,
-      buyOrder: firstOrderCost.value,
-      sellOrder: 0,
-      profit: 0,
-    };
-    recalculateOrder(initialOrder, null);
-    block.orders.push(initialOrder);
+  const removeOrderBlock = (index) => {
+    orderBlocks.value.splice(index, 1);
+    // Update numbering
+    orderBlocks.value.forEach((block, idx) => (block.nr = idx + 1));
+  };
 
-    return block;
-  }
+  const addOrderToBlock = (block) => {
+    const previousOrder = block.orders[block.orders.length - 1];
+    const newBuyOrder = previousOrder
+      ? (parseFloat(previousOrder.buyOrder) * coefNextBuyOrder.value).toFixed(2)
+      : (deposit.value * riskTrade.value).toFixed(2);
 
-  function recalculateOrder(order, previousOrder) {
-    if (previousOrder) {
-      order.buyOrder = +(
-        previousOrder.buyOrder * coefNextOrderCost.value
-      ).toFixed(2);
-    } else {
-      order.buyOrder = +firstOrderCost.value.toFixed(2);
-    }
-    order.tokenAmount = +(order.buyOrder / order.buyPrice).toFixed(2);
-    order.sellOrder = +(
-      order.sellPrice *
-      order.tokenAmount *
-      (1 - fee)
-    ).toFixed(2);
-    order.profit = +(order.sellOrder - order.buyOrder).toFixed(2);
-  }
-
-  function addBlock() {
-    const newBlock = createNewBlock();
-    activeBlocks.value.unshift(newBlock);
-  }
-
-  function addOrder(block) {
-    const lastOrder = block.orders[block.orders.length - 1];
-    const nextBuyPrice = +(lastOrder.buyPrice - 0.1).toFixed(4);
     const newOrder = {
-      id: block.orders.length + 1,
-      buyPrice: nextBuyPrice,
-      sellPrice: +(nextBuyPrice + 0.2).toFixed(4),
-      tokenAmount: 0,
-      buyOrder: 0,
-      sellOrder: 0,
-      profit: 0,
+      buyPrice: 1,
+      sellPrice: null,
+      amount: null,
+      buyOrder: newBuyOrder,
+      sellOrder: null,
+      profit: null,
     };
-    recalculateOrder(newOrder, lastOrder);
-    block.orders.push(newOrder);
-  }
 
-  function removeBlock(blockId) {
-    activeBlocks.value = activeBlocks.value.filter(
-      (block) => block.id !== blockId
+    watchOrderValues(newOrder);
+    block.orders = [...block.orders, newOrder];
+  };
+
+  const removeOrder = (blockIndex, orderIndex) => {
+    const block = orderBlocks.value[blockIndex];
+    if (block && block.orders.length > orderIndex) {
+      block.orders.splice(orderIndex, 1);
+    }
+  };
+
+  const watchOrderValues = (order) => {
+    watch(
+      () => [order.buyPrice],
+      () => {
+        updateOrderValues(order);
+      },
+      { deep: true, immediate: true }
     );
-  }
+  };
+
+  const updateOrderValues = (order) => {
+    if (!order.buyPrice) {
+      order.amount = null;
+      order.buyOrder = null;
+      order.sellOrder = null;
+      order.profit = null;
+      return;
+    }
+
+    const calculateDigits = (priceValue) => {
+      if (priceValue >= 10000) return 1;
+      if (priceValue >= 1000) return 2;
+      if (priceValue >= 100) return 2;
+      if (priceValue >= 10) return 3;
+      if (priceValue >= 1) return 3;
+      if (priceValue >= 0.1) return 4;
+      return 5;
+    };
+
+    const calculateDigitsLote = (digitsPrice) => {
+      if (digitsPrice === 1) return 3;
+      if (digitsPrice === 2) return 2;
+      if (digitsPrice === 3) return 1;
+      if (digitsPrice >= 4) return 0;
+    };
+
+    const digits = calculateDigits(order.buyPrice);
+    const digitsLote = calculateDigitsLote(digits);
+    const buyOrder = (deposit.value * riskTrade.value * (1 + fee)).toFixed(2);
+    const amount = (buyOrder / order.buyPrice).toFixed(digitsLote);
+    const profit = ((order.sellPrice - order.buyPrice) * amount).toFixed(2);
+    const sellOrder = (amount * order.sellPrice * (1 - fee)).toFixed(2);
+
+    order.amount = amount;
+    order.buyOrder = buyOrder;
+    order.sellOrder = sellOrder;
+    order.profit = profit;
+  };
+
+  const clearBuyPrice = (order) => {
+    if (order.buyPrice === 1) order.buyPrice = null;
+  };
+
+  const restoreDefaultBuyPrice = (order) => {
+    if (!order.buyPrice) order.buyPrice = 1;
+  };
 
   return {
+    orderBlocks,
     deposit,
-    coefOfRisk,
-    coefNextOrderCost,
-    firstOrderCost,
-    activeBlocks,
-    addBlock,
-    addOrder,
-    removeBlock,
+    riskTrade,
+    coefNextBuyOrder,
+    addOrderBlock,
+    removeOrderBlock,
+    addOrderToBlock,
+    removeOrder,
+    updateOrderValues,
+    clearBuyPrice,
+    restoreDefaultBuyPrice,
   };
 },
-	{
-		persist: false
-	}
+  { persist: false },
 );
--- src/views/SpotBinance.vue
+++ src/views/SpotBinance.vue
@@ -1,130 +1,12 @@
 <script setup>
-import { ref, computed, watch, reactive } from "vue";
+import { useSpotBinanceStore } from '../stores/spotBinance.js';
 import FooterBinance from '../components/FooterBinance.vue';
+import { storeToRefs } from 'pinia';
 
-// Constants and reactive variables
-const fee = 0.001;
-const deposit = ref(468);
-const riskTrade = ref(0.02);
-const coefNextBuyOrder = ref(1.2);
+const { orderBlocks, deposit,	riskTrade, coefNextBuyOrder, } = storeToRefs(useSpotBinanceStore());
+const { addOrderBlock, removeOrderBlock, addOrderToBlock, removeOrder,
+	updateOrderValues, clearBuyPrice,	restoreDefaultBuyPrice, } = useSpotBinanceStore();
 
-const defaultBlock = () => ({
-	nr: 1,
-	symbol: '',
-	start: null,
-	fin: null,
-	buyPrice: 1,
-	sellPrice: null,
-	orders: reactive([
-		{
-			buyPrice: 1,
-			sellPrice: null,
-			amount: null,
-			buyOrder: null,
-			sellOrder: null,
-			profit: null,
-		},
-	]),
-});
-
-const orderBlocks = ref([defaultBlock()]);
-
-// Helper Functions
-const addOrderBlock = () => {
-	orderBlocks.value.push({ ...defaultBlock(), nr: orderBlocks.value.length + 1 });
-};
-
-const watchOrderValues = (order) => {
-	watch(
-		() => [order.buyPrice], // Watch both buyPrice and sellPrice
-		() => {
-			updateOrderValues(order); // Recalculate whenever either changes
-		},
-		{ deep: true, immediate: true } // Run immediately on initialization
-	);
-};
-
-const addOrderToBlock = (block) => {
-	const previousOrder = block.orders[block.orders.length - 1]; // Get the last order in the block
-	const newBuyOrder = previousOrder
-		? (parseFloat(previousOrder.buyOrder) * coefNextBuyOrder.value).toFixed(2)
-		: (deposit.value * riskTrade.value).toFixed(2); // Use initial 'First Order' value for the first order
-	const newOrder = {
-		buyPrice: 1,
-		sellPrice: null,
-		amount: null,
-		buyOrder: newBuyOrder,
-		sellOrder: null,
-		profit: null,
-	};
-	watchOrderValues(newOrder);
-	//block.orders.push(newOrder);
-	block.orders = [...block.orders, newOrder];
-};
-
-const clearBuyPrice = (order) => {
-	if (order.buyPrice === 1) order.buyPrice = null;
-};
-
-const restoreDefaultBuyPrice = (order) => {
-	if (!order.buyPrice) order.buyPrice = 1;
-};
-
-// Computed for individual blocks
-const calculateDigits = (priceValue) => {
-	if (priceValue >= 10000) return 1;
-	if (priceValue >= 1000) return 2;
-	if (priceValue >= 100) return 2;
-	if (priceValue >= 10) return 3;
-	if (priceValue >= 1) return 3;
-	if (priceValue >= 0.1) return 4;
-	return 5;
-};
-
-const calculateDigitsLote = (digitsPrice) => {
-	if (digitsPrice === 1) return 3;
-	if (digitsPrice === 2) return 2;
-	if (digitsPrice === 3) return 1;
-	if (digitsPrice >= 4) return 0;
-};
-
-const updateOrderValues = (order) => {
-	if (!order.buyPrice) {
-		order.amount = null;
-		order.buyOrder = null;
-		order.sellOrder = null;
-		order.profit = null;
-		return;
-	}
-	const digits = calculateDigits(order.buyPrice);
-	const digitsLote = calculateDigitsLote(digits);
-	const buyOrder = (deposit.value * riskTrade.value * (1 + fee)).toFixed(2);
-	const amount = (buyOrder / order.buyPrice).toFixed(digitsLote);
-	const profit = ((order.sellPrice - order.buyPrice) * amount).toFixed(2);
-	const sellOrder = (amount * order.sellPrice * (1 - fee)).toFixed(2);
-	// Update order values
-	order.amount = amount;
-	order.buyOrder = buyOrder;
-	order.sellOrder = sellOrder;
-	order.profit = profit;
-};
-// Set up watchers for existing orders
-orderBlocks.value.forEach((block) =>
-	block.orders.forEach((order) => watchOrderValues(order))
-);
-
-const removeOrderBlock = (index) => {
-	orderBlocks.value.splice(index, 1);
-	// Update numbering after removing
-	orderBlocks.value.forEach((block, idx) => (block.nr = idx + 1));
-};
-
-const removeOrder = (blockIndex, orderIndex) => {
-	const block = orderBlocks.value[blockIndex]; // Get the specific block
-	if (block && block.orders.length > orderIndex) {
-		block.orders.splice(orderIndex, 1); // Remove the order at the specified index
-	}
-};
 </script>
 
 <template>
@@ -150,12 +32,14 @@ const removeOrder = (blockIndex, orderIndex) => {
 				</label>
 			</div>
 			<div>First Order: {{ (deposit * riskTrade).toFixed(2) }}</div>
-			<button @click="addOrderBlock" class="h-8 px-2 text-sm border border-green-600 font-bold text-green-600">ADD</button>
+			<button @click="addOrderBlock"
+				class="h-8 px-2 text-sm border border-green-600 font-bold text-green-600">
+				ADD
+			</button>
 		</div>
 		<hr class="border-green-600">
 		<!-- Dynamic Orders Blocks -->
 		<div v-for="(block, index) in orderBlocks" :key="block.nr" class="p-2 text-sm">
-			<!-- Block Header -->
 			<div class="flex justify-between items-center">
 				<span>{{ block.nr }}</span>
 				<input id="symbol" type="text" v-model="block.symbol" placeholder="Symbol"
@@ -163,19 +47,20 @@ const removeOrder = (blockIndex, orderIndex) => {
 				<input id="start" type="date" v-model="block.start" class="w-[10ch] bg-gray-900 border text-center" />
 				<input id="fin" type="date" v-model="block.fin" class="w-[10ch] bg-gray-900 border text-center" />
 				<button id="removeBlock" @click="removeOrderBlock(index)"
-					class="px-2 font-bold text-red-600 border border-red-600">X Block</button>
+					class="px-2 font-bold text-red-600 border border-red-600">
+					X Block
+				</button>
 				<button id="addOrder" @click="addOrderToBlock(block)"
 					class="px-2 border border-green-600 font-extrabold text-green-600">
 					+
 				</button>
 			</div>
-			<!--  Orders List -->
-			<div class="">
+			<div>
 				<div v-for="(order, orderIndex) in block.orders" :key="orderIndex" class="flex justify-between mb-1">
 					<span>{{ orderIndex + 1 }}</span>
 					<input id="buyPrice" type="number" v-model="order.buyPrice" placeholder="BuyPrice" min="0.01"
 						class="w-[6ch] bg-gray-900 text-center text-yellow-400" @focus="clearBuyPrice(order)"
-						@blur="restoreDefaultBuyPrice(order)" @input="updateOrderValues(order)" />
+						@blur="restoreDefaultBuyPrice(order)" @change="updateOrderValues(order)" />
 					<span>{{ order.amount }}</span>
 					<span>{{ order.buyOrder }}</span>
 					<input id="sellPrice" type="number" v-model="order.sellPrice" placeholder="SellPrice"
@@ -183,7 +68,9 @@ const removeOrder = (blockIndex, orderIndex) => {
 					<span>{{ order.sellOrder }}</span>
 					<span>{{ order.profit }}</span>
 					<button @click="removeOrder(index, orderIndex)"
-						class="px-2 font-bold text-red-600 border border-red-600">X</button>
+						class="px-2 font-bold text-red-600 border border-red-600">
+						X
+					</button>
 				</div>
 				<hr class="border-green-600">
 			</div>
