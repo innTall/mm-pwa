6333c4b6b601d7a5b6970cc1d2e8ef6850e5bb55
innTall <avg999.trade@gmail.com>
2024-12-26 19:25:52 +0100

    v.0.2.1 Connection with Telegram channel

--- src/components/OrdersBlock.vue
+++ src/components/OrdersBlock.vue
@@ -4,9 +4,9 @@ import MarginOrder from './MarginOrder.vue';
 import { useOrdersBlockStore } from '@/stores/ordersBlock.js';
 import { useModalRemoveStore } from "@/stores/modalRemove.js";
 const { openDialog } = useModalRemoveStore();
-const { showModalRemove, confirmMessage, } = storeToRefs(useModalRemoveStore());
 const { blocks } = storeToRefs(useOrdersBlockStore());
-const { addBlock, removeBlock, addOrder } = useOrdersBlockStore();
+const { removeBlock, addOrder } = useOrdersBlockStore();
+const telegramChannelUrl = "https://t.me/grids_to_long"; // Replace with your Telegram channel URL
 const openRemoveBlockDialog = (blockId) => {
 	openDialog("Delete this block?", () => {
 		removeBlock(blockId);
@@ -21,7 +21,17 @@ const openRemoveBlockDialog = (blockId) => {
 				class="w-[8ch] text-center font-bold bg-gray-900 border uppercase" />
 			<div class="">TotalBalance</div>
 			<div class="">Reserv</div>
-			<button id="tg" class="px-2 bg-gray-700">TG</button>
+			<div class="flex items-center space-x-4">
+				<a :href="telegramChannelUrl" target="_blank" rel="noopener noreferrer"
+					class="text-blue-500 hover:text-blue-700">
+					<!-- Telegram Icon SVG -->
+					<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 border border-blue-500 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+						stroke-width="3">
+						<path
+							d="M21.743 3.263a1.254 1.254 0 0 0-1.396-.026L3.88 11.394c-.355.215-.572.574-.57.98.002.406.222.765.578.977l4.064 2.357c.261.15.578.142.832-.019l2.95-1.844 2.142 2.16c.168.169.422.241.66.185a.786.786 0 0 0 .504-.456l3.73-11.942a.788.788 0 0 0-.007-.51z" />
+					</svg>
+				</a>
+			</div>
 			<button @click="openRemoveBlockDialog(block.id)" class="px-2 font-bold text-red-600 border border-red-600">
 				X
 			</button>
